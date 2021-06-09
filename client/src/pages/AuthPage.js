d37249ed677ec2694ec446b630cf06c79eea814f
EugeniySichev <sichev91@inbox.ru>
2021-06-09 21:24:29 +0300

    add basic ui

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -2,8 +2,48 @@ import React from 'react'
 
 export const AuthPage = () => {
     return(
-        <div>
-            <h1>Auth page</h1>
+        <div className="row">
+            <div className="col s6 offset-s3">
+                <h1>Сократи ссылку</h1>
+                <div className="card blue darken-1">
+        <div className="card-content white-text">
+          <span className="card-title">Авторизация</span>
+          <div>
+
+              <div className="input-field">
+                  <input 
+                  placeholder="введите email" 
+                  id="email" 
+                  type="text"
+                  name="email"
+                  className="yellow-input"
+                  />
+
+                  
+                      <label htmlFor="email">Email</label>
+              </div>
+
+              <div className="input-field">
+                  <input 
+                  placeholder="введите пароль" 
+                  id="password" 
+                  type="password"
+                  name="password"
+                  className="yellow-input"
+                  />
+
+                  
+                      <label htmlFor="email">Пароль</label>
+              </div>
+
+          </div>
+        </div>
+        <div className="card-action">
+            <button className="btn yellow darken-4" style={{marginRight: 10}}>Войти</button>
+            <button className="btn lighten-1 black-text">Регистрация</button>
+        </div>
+      </div>
+            </div>
         </div>
     )
 }
